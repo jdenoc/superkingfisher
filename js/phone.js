3fa4af32fa1e7e77d902c62840818b14310005f4
Dom Scandinaro <dominic.scandinaro@eprize.com>
2013-10-05 18:12:57 -0500

    basic box "working"

--- js/phone.js
+++ js/phone.js
@@ -3,4 +3,14 @@ window.ondeviceorientation = function(e) {
     tiltXAxis = parseFloat( e.beta );
     tileYAxis = parseFloat( e.alpha );
     $("#vals").html(tiltXAxis+","+tileYAxis);
+    $.ajax({
+        type: 'GET',
+        url: './move.php?x='+tiltXAxis+'&y='+tileYAxis,
+        cache: false,
+        beforeSend:function(){ },
+        success:function(data){
+
+        },
+        error:function(){ }
+    });
 }
