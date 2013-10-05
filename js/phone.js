518e0a9cd588fad097470a81afeeea539f4475f2
Denis O'Connor <denis.oconnor@eprize.com>
2013-10-05 18:39:36 -0500

    Corrected X-Axis and Y-Axis information that is transfered. Created a .gitignore file.

--- js/phone.js
+++ js/phone.js
@@ -1,11 +1,11 @@
 window.ondeviceorientation = function(e) {
     tiltZAxis = parseFloat( e.gamma );
-    tiltXAxis = parseFloat( e.beta );
-    tileYAxis = parseFloat( e.alpha );
-    $("#vals").html(tiltXAxis+","+tileYAxis);
+    tiltXAxis = parseFloat( e.beta ) * -1;
+    tiltYAxis = parseFloat( e.alpha );
+    $("#vals").html(tiltXAxis+"<br/>"+tiltYAxis+"<br/>"+tiltZAxis);
     $.ajax({
         type: 'GET',
-        url: './move.php?x='+tiltXAxis+'&y='+tileYAxis,
+        url: './move.php?x='+tiltXAxis+'&y='+tiltZAxis,
         cache: false,
         beforeSend:function(){ },
         success:function(data){
