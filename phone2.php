36350650b3d5a011724b21c8460d15c00cffc0da
Denis O'Connor <denis.oconnor@eprize.com>
2013-10-06 13:21:32 -0500

    Remove un-needed code from phone2.php

--- phone2.php
+++ phone2.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PLayer 2</title>
-    <link rel='stylesheet' href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/themes/base/jquery-ui.css' type='text/css' media='screen' />
     <link rel='stylesheet' href='css/style.css' type='text/css' media='screen' />
-    <script src="http://www.google.com/jsapi" type="text/javascript"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
     <script src="js/phone2.js?x=2" type="text/javascript"></script>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div id="phone_rod2"></div>
